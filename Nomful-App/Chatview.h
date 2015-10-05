febc51fcb55171f41dcd16346e094da2fca79cbe
seancrowe5 <sean.crowe@me.com>
2015-10-05 13:26:14 -0500

    added firebase files

---
+++ Nomful-App/Chatview.h
@@ -0,0 +1,20 @@
+//
+//  Chatview.h
+//  Nomful-App
+//
+//  Created by Sean Crowe on 9/28/15.
+//  Copyright © 2015 Nomful, inc. All rights reserved.
+//
+
+#import "JSQMessages.h"
+#import "LogMealSinglePageViewController.h"
+
+
+
+
+@interface Chatview : JSQMessagesViewController
+
+- (id)initWith:(NSString *)groupId_;
+
+
+@end
